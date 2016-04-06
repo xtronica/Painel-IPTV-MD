ff0fea49a3d2e41925991737d2a1c9d26ca47d3b
vaniusha3 <dj_vaniusha@mail.ru>
2016-04-06 21:29:54 +0300

    Update ffmpeg.sh

--- ffmpeg.sh
+++ ffmpeg.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo "Se Instaleaza FFmpeg"
-wget http://johnvansickle.com/ffmpeg/releases/ffmpeg-release-64bit-static.tar.xz -O /home/fos-streaming/ffmpeg-release-64bit-static.tar.xz
+wget http://johnvansickle.com/ffmpeg/releases/ffmpeg-release-64bit-static.tar.xz
 /bin/mkdir /usr/src/ffmpeg
-tar -xJf /home/fos-streaming/ffmpeg-release-64bit-static.tar.xz -C /usr/src/ffmpeg
+tar -xJf ffmpeg-release-64bit-static.tar.xz -C /usr/src/ffmpeg
 /bin/cp /usr/src/ffmpeg/ffmpeg*/ffmpeg  /usr/bin/ffmpeg
 /bin/cp /usr/src/ffmpeg/ffmpeg*/ffprobe /usr/bin/ffprobe
 chmod 755 /usr/bin/ffmpeg
